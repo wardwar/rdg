0019c5fe1153d272d41a8d265a40a0dcbff97e18
wildan angga <wildan.aga@gmail.com>
2014-12-27 18:01:40 +0700

    first

---
+++ index.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>RDGordyn</title>
+
+    <!-- Bootstrap -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/style.css" rel="stylesheet">
+    <link href="assets/css/font.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+
+  <!-- header -->
+  <header class="bg-red header-red">
+    <nav class="bg-default navbar"></nav>
+    <h1 class="title">RDGordyn</h1>
+    <p class="color-white text-center">
+      Hias ruangan favorite anda dengan gordyn dari kami, harga dan kualitas bersaing.<br/>
+      Tunggu apa lagi segera hubungi kami dan pesan gordyn keinginan anda
+    </p>
+    
+    <button class="btn btn-lg center-block bg-button btn-hub">Hubungi Kami</button>
+  </header>
+  <!-- end header -->
+
+  <!-- icon -->
+  <div class="container con-icon">
+    <div class="container">
+      <div class="col-md-4 col-sm-4 col-xs-12 icon">
+        <img class="center-block" src="img/curtain.png">
+        <h4 class="text-center">Model Terbaru</h3>
+      </div>
+
+      <div class="col-md-4 col-sm-4 col-xs-12 icon">
+        <img class="center-block" src="img/kain.png">
+        <h4 class="text-center">Bahan Kain Berkualitas</h3>
+      </div>
+
+      <div class="col-md-4 col-sm-4 col-xs-12 icon">
+        <img class="center-block" src="img/accessor.png">
+        <h4 class="text-center">Accessories Lengkap</h3>
+      </div>
+    </div>
+  </div>
+
+  <!-- end icon -->
+
+  <div class="con-sample-cloth">
+    <div class="container">
+      <div class="col-md-4 col-xs-12">
+        <div class="kain-kategory shadow">
+          <img src="img/mantie.png">
+        </div>
+        <div class="kain-title">
+            <h3>Perkantoran</h3>
+        </div>
+        <div class="kain-description">
+          <img src="img/curtain3.png" alt="Office Curtain">
+          <p>
+            Hias ruang perkantoran anda dengan gordyn yang minimalis namun enak di pandang
+            untuk menunjang produktivitas pegawai. Pemilihan wana formal menjadi kesan
+            terhormat di sebuah ruangan. RDGordyn siap untuk memenuhi kebutuhan dekorasi kantor anda
+            dengan gordyn yang simple dan minimalis.
+          </p>
+        </div>
+      </div>
+
+    <div class="col-md-4  col-xs-12">
+        <div class="kain-kategory shadow">
+          <img src="img/wooman.png">
+        </div>
+        <div class="kain-title">
+            <h3>Ruang Tamu</h3>
+        </div>
+        <div class="kain-description">
+        <img src="img/curtain2.png" alt="Office Curtain">
+          <p>
+            Ruang tamu dan ruang keluarga sangat dibutuhkan untuk mereflesikan diri dari
+            kepenatan setelah bekerja, hias ruang keluarga anda dengan gordyn dari RDGordyn
+            untuk memberi kehangatan pada keluarga anda.
+          </p>
+        </div>
+      </div>
+
+    <div class="col-md-4  col-xs-12">
+        <div class="kain-kategory shadow">
+          <img src="img/man.png">
+        </div>
+        <div class="kain-title">
+            <h3>Kamar Anak</h3>
+        </div>
+        <div class="kain-description">
+        <img src="img/curtain1.png" alt="Office Curtain">
+          <p>
+            Kamar anak harus di desain sesuai kegemaran atau karakter anak, sehingga anak bisa betah
+            di dalam kamar. Selain membuat anak jadi senang ruangan jadi lebih enak untuk di pandang apabila desain nya bagus. Hias ruang kamar tidur anak dengan gordyn dari RDGordyn.
+          </p>
+        </div>
+      </div>
+    </div>
+      
+  </div>
+
+
+  <div class="con-sample-acs">
+  <div class="container">
+  <div class="col-md-4">
+    
+  </div>
+    <div class="col-md-8">
+      <h1 class="section text-right">
+        Accessories
+      </h1>
+      <p class="text-right">
+        Untuk memperindah gordyn anda, accessories menjadi salah satu yang dapat
+        memberi kesan simple, glamore, dan meriah.
+      </p>
+      <p class="text-right">
+        Accessories mulai dari......
+      </p>
+    </div>
+    </div>
+  </div>
+
+
+
+  <div class="con-sample-project bg-white">
+    <div class="container">
+      <h1>Our Project</h1>
+
+      <div class="col-md-3">
+        <div class="category-project shadow">
+          
+        </div>
+      </div>
+      <div class="col-md-8">
+        <div class="col-md-6">
+          <div class="project shadow">
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="project shadow">
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="project shadow">
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="project shadow">
+          </div>
+        </div>
+         <div class="col-md-6">
+          <div class="project shadow">
+          </div>
+        </div>
+        <div class="col-md-6">
+          <div class="project shadow">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <footer class="bg-default">
+    <div class="foot-pallete bg-footer">
+      
+    </div>
+  </footer>
+
+
+
+
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
